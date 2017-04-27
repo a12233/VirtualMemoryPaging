ac4aead88ea041196a2ca32a009fb16b409626bd
lirex14 <lirex14@gmail.com>
2017-04-26 21:14:28 -0400

    dyn allocate arrays

--- VMPaging/VMPaging/memoryManager.hpp
+++ VMPaging/VMPaging/memoryManager.hpp
@@ -14,19 +14,23 @@ public:
 	memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA) :
 		virtualMemoryManagerInterface(p, pS, nF, vA) {
 		// initialize page table
+			PAGE_TABLE = (unsigned int*)calloc(nF, 8);
+			PAGE_TABLE_VALID = (bool*)calloc(nF, 1);
+
 			for (unsigned int i = 0; i < nF; i++) {
-				PAGE_TABLE[i] = 0;
-				// no memory-page assigned == false
 				PAGE_TABLE_VALID[i] = false;
 			}
 
 			// initialize physical memory
 			phyMemSize = numFrames * pow(2 , N);
+			PHYSICAL_MEMORY = (int*)calloc(phyMemSize, 8); 
+			PHYSICAL_MEMORY_TIME_IN = (int*)calloc(phyMemSize, 8);
+			PHYSCIAL_MEMORY_TIME_ACCESS = (int*)calloc(phyMemSize, 8);
+			PHYSICAL_MEMORY_FREE = (bool*)calloc(phyMemSize, 1);
+
 			for (int i = 0; i < phyMemSize; i++) {
-				PHYSICAL_MEMORY[i] = 0;
 				PHYSICAL_MEMORY_FREE[i] = true;
-				PHYSICAL_MEMORY_TIME_IN[i] = 0;
-				PHYSCIAL_MEMORY_TIME_ACCESS[i] = 0;
+				
 			}
 	
 	};
@@ -47,7 +51,7 @@ private:
 	unsigned int * PAGE_TABLE;
 	bool *PAGE_TABLE_VALID;
 
-	unsigned int * PHYSICAL_MEMORY;
+	int * PHYSICAL_MEMORY;
 	bool * PHYSICAL_MEMORY_FREE;
 	int * PHYSICAL_MEMORY_TIME_IN;
 	int * PHYSCIAL_MEMORY_TIME_ACCESS;
--- VMPaging/VMPaging/virtualMemPaging.cpp
+++ VMPaging/VMPaging/virtualMemPaging.cpp
@@ -7,9 +7,12 @@
 int main()
 {
 	//	memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA) :
-
+	//virtualMemoryManagerInterface(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA) : policy(p), N(pS), numFrames(nF), virtualAddressSpaceSize(vA) {
+		//assert(virtualAddressSpaceSize > N);
+		//assert(pow(2, virtualAddressSpaceSize) > numFrames * pow(2, N));
+	
 	int frames[12] = { 0,2,1,6,4,0,1,0,3,1,2,1 };
-	memoryManager mem1(FIFO, 4, 4, 20);
+	memoryManager mem1(FIFO, 4, 4, 100);
 
 	for (int i = 0; i < 12; i++)
 	{
